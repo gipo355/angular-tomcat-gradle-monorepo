1451249a4ec6f711042ff8ff8b66e34923a7ea54
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-07-08 10:09:39 +0200

    WIP add buildable lib

---
+++ libs/angular-lib-three/src/lib/angular-lib-three/angular-lib-three.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-angular-lib-three',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './angular-lib-three.component.html',
+  styleUrl: './angular-lib-three.component.css',
+})
+export class AngularLibThreeComponent {}
